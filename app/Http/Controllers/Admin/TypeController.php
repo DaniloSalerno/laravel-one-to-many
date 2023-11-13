d7c0adb14b255f46824f3fc2cbb6cdd0f2992a04
DaniloSalerno <salerno.danilo@hotmail.it>
2023-11-13 17:22:51 +0100

    add destroy() method in TypeController

--- app/Http/Controllers/Admin/TypeController.php
+++ app/Http/Controllers/Admin/TypeController.php
@@ -77,6 +77,8 @@ class TypeController extends Controller
      */
     public function destroy(Type $type)
     {
-        //
+        $type->delete();
+
+        return to_route('admin.types.index')->with('message', 'Welldone! Types deleted successfully');
     }
 }
